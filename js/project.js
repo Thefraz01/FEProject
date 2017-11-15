480e3f33ac6b766e7edae559caad5597800c3645
Greg Frazier <thefraz.gf@gmail.com>
2017-11-15 17:07:53 -0500

    Wednesday

--- js/project.js
+++ js/project.js
@@ -2,34 +2,10 @@
 
 /* submit button pop-up */
 
-function clicked(e)
-{
-    if(!confirm('Ah, so are you sure you want me to keep bugging you?'))e.preventDefault();
+  	function clicked(e) 
+{var name = document.getElementsByName("name")[0].value;
+    if(!confirm("Ah, " + name + ", you sure you want me to keep bugging you?"))e.preventDefault();
 }
 
 
 
-
-// Create the dropdown base
-$("<select />").appendTo("nav");
-
-// Create default option "Go to..."
-$("<option />", {
-   "selected": "selected",
-   "value"   : "",
-   "text"    : "Go to..."
-}).appendTo("nav select");
-
-// Populate dropdown with menu items
-$("nav a").each(function() {
- var el = $(this);
- $("<option />", {
-     "value"   : el.attr("href"),
-     "text"    : el.text()
- }).appendTo("nav select");
-});
-
-
-$("nav select").change(function() {
-  window.location = $(this).find("option:selected").val();
-});
